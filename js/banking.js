d2784609e0f3b7834d5a78fc2db1e0e7e2363e59
ab-alamin <abdullahalamin9876@gmail.com>
2021-09-03 12:27:33 +0600

    js edited

--- js/banking.js
+++ js/banking.js
@@ -1,72 +1,72 @@
-// // handle deposite button event
-// document.getElementById('deposite-button').
-// addEventListener('click', function(){
-//     // get the amount deposited
-//     const depositeInpute = document.getElementById('deposite-input');
+// handle deposite button event
+document.getElementById('deposite-button').
+addEventListener('click', function(){
+    // get the amount deposited
+    const depositeInpute = document.getElementById('deposite-input');
 
-//     const newDepositeAmountText = depositeInpute.value;
-//     const newDepositeAmount = parseFloat(newDepositeAmountText);
-//     // console.log(depositeAmount);
+    const newDepositeAmountText = depositeInpute.value;
+    const newDepositeAmount = parseFloat(newDepositeAmountText);
+    // console.log(depositeAmount);
 
-//     //  update deposite total
-
-
-//     const depositeTotal = document.getElementById('deposite-total')
-//     ;
-//     const previusDepositeText = depositeTotal.innerText;
-//     const previusDepositeAmount = parseFloat(previusDepositeText);
-
-//     const newdepositeTotal =  previusDepositeAmount + newDepositeAmount;
+    //  update deposite total
 
 
-//     depositeTotal.innerText = newdepositeTotal;
+    const depositeTotal = document.getElementById('deposite-total')
+    ;
+    const previusDepositeText = depositeTotal.innerText;
+    const previusDepositeAmount = parseFloat(previusDepositeText);
+
+    const newdepositeTotal =  previusDepositeAmount + newDepositeAmount;
 
 
-// // update account balace 
-
-// const balanceTotal = document.getElementById('balance-total');
-
-// const balanceTotalText = balanceTotal.innerText;
-// const previusBalanceTotal = parseFloat(balanceTotalText);
-// const newBalanceTotal = previusBalanceTotal + newDepositeAmount;
-// balanceTotal.innerText = newBalanceTotal;
+    depositeTotal.innerText = newdepositeTotal;
 
 
+// update account balace 
+
+const balanceTotal = document.getElementById('balance-total');
+
+const balanceTotalText = balanceTotal.innerText;
+const previusBalanceTotal = parseFloat(balanceTotalText);
+const newBalanceTotal = previusBalanceTotal + newDepositeAmount;
+balanceTotal.innerText = newBalanceTotal;
 
 
-//     //  clear the deposite inpute field
-//     depositeInpute.value = '';
-// })
-
-// // handle event withdraw handler
-
-// document.getElementById('withdraw-button').addEventListener
-// ('click', function () {
-//     const withdrawInput = document.getElementById('withdraw-input');
-//     const withdrawAmountText = withdrawInput.value;
-//     const newWithdrawAmount = parseFloat(withdrawAmountText);
-
-//     // set withdraw total
-//     const withDrawTotal = document.getElementById('withdeaw-total');
-//     const previousWithDrawText = withDrawTotal.innerText;
-//     const previousWithDrawTotal = parseFloat(previousWithDrawText);
-
-//     const newWithdrawTotal = previousWithDrawTotal + newWithdrawAmount;
-//     withDrawTotal.innerText = newWithdrawTotal;
 
 
-//     // update balance
+    //  clear the deposite inpute field
+    depositeInpute.value = '';
+})
+
+// handle event withdraw handler
+
+document.getElementById('withdraw-button').addEventListener
+('click', function () {
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+
+    // set withdraw total
+    const withDrawTotal = document.getElementById('withdeaw-total');
+    const previousWithDrawText = withDrawTotal.innerText;
+    const previousWithDrawTotal = parseFloat(previousWithDrawText);
+
+    const newWithdrawTotal = previousWithDrawTotal + newWithdrawAmount;
+    withDrawTotal.innerText = newWithdrawTotal;
+
+
+    // update balance
     
-//     const balanceTotal = document.getElementById('balance-total');
-//     const previusBalanceText = balanceTotal.innerText;
-//     const previusBalanceTotal = parseFloat(previusBalanceText);
-//     const newBalanceTotal = previusBalanceTotal - newWithdrawAmount;
-//     balanceTotal.innerText = newBalanceTotal;
-//     //  clear withdraw input
-//     withdrawInput.value = '';
+    const balanceTotal = document.getElementById('balance-total');
+    const previusBalanceText = balanceTotal.innerText;
+    const previusBalanceTotal = parseFloat(previusBalanceText);
+    const newBalanceTotal = previusBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
+    //  clear withdraw input
+    withdrawInput.value = '';
 
     
 
 
 
-// })
+})
